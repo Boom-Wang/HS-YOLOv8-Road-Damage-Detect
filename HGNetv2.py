0acb820f1db020976de4c7d2736ac243e8ec9f54
Boom-Wang <lplsmayhoon@gmail.com>
2025-02-01 18:45:53 +0800

    Add files via upload

---
+++ HGNetv2.py
@@ -0,0 +1,4 @@
+# 论文地址：https://arxiv.org/abs/2304.08069
+# 代码地址：https://github.com/PaddlePaddle/PaddleDetection
+# 中文翻译：https://blog.csdn.net/weixin_43694096/article/details/131353118
+# 博客地址：https://blog.csdn.net/weixin_43694096/article/details/135737243
